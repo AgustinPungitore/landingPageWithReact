a743f84314959edd206da31d1cf691cfd903c32b
AgustinPungitore <agustinpungitore@gmail.com>
2022-08-18 03:04:01 +0000

    export done, review import

---
+++ src/js/component/01-navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Header (){
+   return <nav className="navbar navbar-dark bg-dark">
+    </nav>
+
+}
+
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,25 +2,21 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import Header from "./01-navbar.jsx";
+import Jumbotron from "./02-jumbotron.jsx";
+import Cards from  "./03-cards.jsx";
+import Footer from "./04-footer.jsx";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	<div>
+	<Header/>
+	<Jumbotron/>
+	<Cards/>
+	<Footer/>
+	</div>
 	);
-};
-
-export default Home;
+	};
+	
+	export default Home;
+	
